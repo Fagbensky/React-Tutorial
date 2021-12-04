67647ed188847386850d19518bf9fe7c5bc41eff
Joshua Fagbenro <foja.digital@gmail.com>
2021-12-04 23:10:56 +0100

    Props destructuring and arrararrayiteration

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const books = [
+  {
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81eB+7+CkUL._AC_UL302_SR302,200_.jpg"
+  },
+  {
+    title: "American Marxism",
+    author:"Mark R. Levin",
+    img:"https://images-na.ssl-images-amazon.com/images/I/81lsmyMcYNL._AC_UL302_SR302,200_.jpg"
+  }
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BookList = () => {
+    return (
+      <section className="bookList">
+        {
+          books.map((book) => {
+            const { img, title, author } = book;
+            return (
+              <Book
+                title={title}
+                author={author}
+                img={img}
+              />
+            );
+          })
+        }
+      </section>
+    );
+};
+
+const Book = ({ img, title, author, children }) => {
+  //const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {children }
+    </article>
+  );
+};
+
+
+ReactDom.render(<BookList />, document.getElementById("root"));
