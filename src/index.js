a51984361e8cd0d35b0aca8494a58a42d25c51b5
Joshua Fagbenro <foja.digital@gmail.com>
2021-12-04 23:32:24 +0100

    Spread Operator

--- src/index.js
+++ src/index.js
@@ -4,15 +4,23 @@ import "./index.css";
 
 const books = [
   {
+    id:1,
     title: "I Love You to the Moon and Back",
     author: "Amelia Hepworth",
-    img: "https://images-na.ssl-images-amazon.com/images/I/81eB+7+CkUL._AC_UL302_SR302,200_.jpg"
+    img: "https://images-na.ssl-images-amazon.com/images/I/81eB+7+CkUL._AC_UL302_SR302,200_.jpg",
   },
   {
+    id:2,
     title: "American Marxism",
-    author:"Mark R. Levin",
-    img:"https://images-na.ssl-images-amazon.com/images/I/81lsmyMcYNL._AC_UL302_SR302,200_.jpg"
-  }
+    author: "Mark R. Levin",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81lsmyMcYNL._AC_UL302_SR302,200_.jpg",
+  },
+  {
+    id:3,
+    title: "It Ends With Us: A Novel",
+    author: "Colleen Hoover",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71xUvuJiqgL._AC_UL604_SR604,400_.jpg",
+  },
 ];
 
 const BookList = () => {
@@ -21,7 +29,7 @@ const BookList = () => {
         {
           books.map((book) => {
             return (
-              <Book book={book}
+              <Book key={book.id} {...book}
               />
             );
           })
@@ -30,8 +38,8 @@ const BookList = () => {
     );
 };
 
-const Book = (props) => {
-  const { img, title, author } = props.book;
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
